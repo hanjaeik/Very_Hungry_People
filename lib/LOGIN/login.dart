4e29677c0e6e5b40dc0d0ecdfbc1595d92c4b1f2
jaeik <hhda84200@gmail.com>
2024-03-26 17:07:01 +0900

    ssad

--- lib/LOGIN/login.dart
+++ lib/LOGIN/login.dart
@@ -10,7 +10,7 @@ class LoginApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Login',
+      title: 'Simple Lsogin',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
